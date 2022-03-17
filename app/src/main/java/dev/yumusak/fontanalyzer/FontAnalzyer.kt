dc5cc9ddbbd964d10c4c9c8ef39b378b226f915b
Can Yumusak <cyumusak@squareup.com>
2022-03-17 22:06:38 +0100

    Small adjustments

--- app/src/main/java/dev/yumusak/fontanalyzer/FontAnalzyer.kt
+++ app/src/main/java/dev/yumusak/fontanalyzer/FontAnalzyer.kt
@@ -1,6 +1,9 @@
 package dev.yumusak.fontanalyzer
 
 import android.graphics.Paint
+import android.text.StaticLayout
+import android.text.TextPaint
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Surface
@@ -11,6 +14,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.drawBehind
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.translate
 import androidx.compose.ui.platform.LocalContext
@@ -29,7 +33,7 @@ import dev.yumusak.fontanalyzer.ui.theme.Typography
 
 @Composable
 fun FontAnalzyer(fontId: Int) = with(LocalDensity.current) {
-    val fontSize = 70.sp
+    val fontSize = 140.sp
     val typeface = ResourcesCompat.getFont(LocalContext.current, fontId)
     val paint = Paint().also {
         it.typeface = typeface
@@ -52,22 +56,45 @@ fun FontMetricsList(
     paint: Paint,
     fontSize: TextUnit,
     font: Int
-) {
+) = with(LocalDensity.current) {
     val fontMetrics = paint.fontMetrics
     Column(
         modifier = Modifier.padding(horizontal = 16.dp),
         verticalArrangement = Arrangement.spacedBy(16.dp)
     ) {
         Metric(Color.Blue, "Top", fontMetrics.top)
-        Metric(Color.Green, "Ascent", fontMetrics.ascent)
-        Metric(Color.Green, "Descent", fontMetrics.descent)
+        Metric(Color.DarkGray, "Ascent", fontMetrics.ascent)
+        Metric(Color.DarkGray, "Descent", fontMetrics.descent)
         Metric(Color.Blue, "Bottom", fontMetrics.bottom)
+
+        val height = StaticLayout.Builder.obtain(
+            "Fylo",
+            0,
+            4,
+            TextPaint(paint),
+            Integer.MAX_VALUE
+        ).build().height
+        Metric(null, "Font size", paint.textSize)
+        Metric(null, "Height", height.toFloat())
     }
 }
 
 @Composable
-private fun ColumnScope.Metric(color: Color, text: String, value: Float) {
-    Row(modifier = Modifier.fillMaxWidth()) {
+private fun ColumnScope.Metric(color: Color?, text: String, value: Float) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if (color != null) {
+            Box(
+                modifier = Modifier
+                    .size(8.dp)
+                    .background(color)
+            )
+            Spacer(modifier = Modifier.size(8.dp))
+        } else {
+            Spacer(modifier = Modifier.size(16.dp))
+        }
         Text(text = text, style = Typography.titleLarge)
         Text(
             textAlign = TextAlign.End,
@@ -89,7 +116,7 @@ fun FontDisplay(
     Box(
         modifier = Modifier
             .fillMaxWidth()
-            .height(200.dp),
+            .height(300.dp),
         contentAlignment = Alignment.Center,
     ) {
         Text(
@@ -111,6 +138,7 @@ fun FontDisplay(
 private fun DrawScope.FontMetrics(fontMetrics: Paint.FontMetrics) {
     drawLine(
         Color.Red,
+        strokeWidth = 3.0f,
         start = Offset(0f, 0f),
         end = Offset(size.width, 0f)
     )
@@ -118,20 +146,23 @@ private fun DrawScope.FontMetrics(fontMetrics: Paint.FontMetrics) {
     // Baseline
     drawLine(
         Color.Red,
+        strokeWidth = 3.0f,
         start = Offset(0f, 0f),
         end = Offset(size.width, 0f)
     )
 
     // Descent
     drawLine(
-        Color.Green,
+        Color.DarkGray,
+        strokeWidth = 3.0f,
         start = Offset(0f, fontMetrics.descent),
         end = Offset(size.width, fontMetrics.descent)
     )
 
     // Ascent
     drawLine(
-        Color.Green,
+        Color.DarkGray,
+        strokeWidth = 3.0f,
         start = Offset(0f, fontMetrics.ascent),
         end = Offset(size.width, fontMetrics.ascent)
     )
@@ -139,6 +170,7 @@ private fun DrawScope.FontMetrics(fontMetrics: Paint.FontMetrics) {
     // Top
     drawLine(
         Color.Blue,
+        strokeWidth = 3.0f,
         start = Offset(0f, fontMetrics.top),
         end = Offset(size.width, fontMetrics.top)
     )
@@ -146,6 +178,7 @@ private fun DrawScope.FontMetrics(fontMetrics: Paint.FontMetrics) {
     // Bottom
     drawLine(
         Color.Blue,
+        strokeWidth = 3.0f,
         start = Offset(0f, fontMetrics.bottom),
         end = Offset(size.width, fontMetrics.bottom)
     )
